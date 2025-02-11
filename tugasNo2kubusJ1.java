cfba634cd3cd10aecb846bf6f306f0a09cc95e88
Lovie-Tonimba <jechonialovie@gmail.com>
2025-02-11 22:24:58 +0700

    rumus rumus kubus

--- tugasNo2kubusJ1.java
+++ tugasNo2kubusJ1.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class kubusJ1 {
+public class tugasNo2kubusJ1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) { 
