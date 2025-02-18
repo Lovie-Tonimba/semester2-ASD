375aab8a1ce8f3d1f8054fd27a6665b456725cdf
Lovie-Tonimba <jechonialovie@gmail.com>
2025-02-18 14:42:15 +0700

    .

--- objectjobsheet2/mahasiswa10J2.java
+++ objectjobsheet2/mahasiswa10J2.java
@@ -4,14 +4,6 @@ public class mahasiswa10J2 {
     String nim;
     String kelas;
     double ipk;
-    public mahasiswa10J2(){
-    }
-    public mahasiswa10J2(String nm, String nim, double ipk, String kls){
-        this.nama = nm;
-        this.nim = nim;
-        this.ipk = ipk;
-        this.kelas = kls;
-    }
     void tampilkanInformasi(){
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -22,7 +14,8 @@ public class mahasiswa10J2 {
         kelas = kelasBaru;
     }
     void updateipk(double ipkBaru){
-        if (ipk >= 0.0 && ipk <= 4.0){
+        if(ipk >= 0.0 && ipk <= 4.0){
+            nilaiKinerja();
         }else{
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
