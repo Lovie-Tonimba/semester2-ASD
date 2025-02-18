a45a12e11669113319082ecb0e2efc708f56a5d3
Lovie-Tonimba <jechonialovie@gmail.com>
2025-02-18 14:35:36 +0700

    .

---
+++ objectjobsheet2/mahasiswa10J2.java
@@ -0,0 +1,42 @@
+package objectjobsheet2;
+public class mahasiswa10J2 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+    public mahasiswa10J2(){
+    }
+    public mahasiswa10J2(String nm, String nim, double ipk, String kls){
+        this.nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kls;
+    }
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+    void updateipk(double ipkBaru){
+        if (ipk >= 0.0 && ipk <= 4.0){
+        }else{
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+        ipk = ipkBaru;
+    }
+    String nilaiKinerja(){
+        if(ipk >= 3.5){
+            return "Kinerja sangat baik";
+        }else if(ipk >= 3.0){
+            return "Kinerja baik";
+        }else if(ipk >= 2.0){
+            return "Kinerja cukup";
+        }else{
+            return "Kinerja kurang";
+        }
+    }
+}
---
+++ objectjobsheet2/mahasiswaMain10J2.java
@@ -0,0 +1,19 @@
+package objectjobsheet2;
+public class mahasiswaMain10J2 {
+    public static void main(String[] args) {
+        mahasiswa10J2 mhs1 = new mahasiswa10J2();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateipk(3.60);
+        mhs1.tampilkanInformasi();
+
+        mahasiswa10J2 mhs2 = new mahasiswa10J2("Annisa Nabila", "2141720171", 3.25, "TI 2L");
+        mhs2.updateipk(3.30);
+        mhs2.tampilkanInformasi(); 
+    }
+}
