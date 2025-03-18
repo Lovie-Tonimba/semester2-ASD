205780888844fe323ed617268dc7e3e65a9fb885
Lovie-Tonimba <jechonialovie@gmail.com>
2025-03-18 12:32:46 +0700

    praktikum 1 bagian b

--- jobsheet6/sortingMain10.java
+++ jobsheet6/sortingMain10.java
@@ -2,12 +2,20 @@ package jobsheet6;
 public class sortingMain10 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
         sorting10 dataurut1 = new sorting10(a, a.length);
+        sorting10 dataurut2 = new sorting10(b, b.length);
+    
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
-
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+        
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
